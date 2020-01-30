773bdcd1d00bb209509c6188cb626ded3e3f7b2a
Zahrachab <32749680+Zahrachab@users.noreply.github.com>
2020-01-30 15:51:47 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1QAyQ1v_X2eCsO5gMTmwSycUqP_0xGKiw/view?usp=sharing'
+export_file_url = 'https://www.dropbox.com/s/wcgoqrj4nwi65ps/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['erreur', 'autre']
